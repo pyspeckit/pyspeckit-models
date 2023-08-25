c87d40b92df7b271cd70c152a6c8f7f13d236aae
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-08-25 15:20:38 -0400

    add note about JPW notebook

--- pyspeckitmodels/co/exomol_co_vibration.py
+++ pyspeckitmodels/co/exomol_co_vibration.py
@@ -6,6 +6,9 @@ References:
 
     https://hitran.org/docs/definitions-and-units/
     https://ui.adsabs.harvard.edu/abs/2018A%26A...614A.131Y/abstract
+
+See also
+https://nbviewer.org/url/interstellarmedium.github.io/molecules/CO_rovib.ipynb
 """
 
 import warnings
